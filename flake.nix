fabefcfda40b38f8744710345ceca94107db11f9
Anselm Schüler <mail@anselmschueler.com>
2022-05-09 19:24:52 +0200

    add epoxy

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
 
           flutter-deps = optional enable-android androidComposition.androidsdk
             ++ optList enable-linuxDesktop
-            (with pkgs; [ clang cmake ninja pkg-config gtk3.dev pcre ]);
+            (with pkgs; [ clang cmake ninja pkg-config gtk3.dev pcre epoxy ]);
 
           flutter-fhs = pkgs.buildFHSUserEnv {
             name = "flutter";
